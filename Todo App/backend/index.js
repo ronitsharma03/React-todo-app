b9166dd407df87f2471776ae968fbd092afdf1e6
ronitsharma03 <ronitkhajuria03>
2024-03-14 10:08:07 +0530

    fixed index.js

--- Todo App/backend/index.js
+++ Todo App/backend/index.js
@@ -5,9 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/todos', function(req, res){
-    res.json({
-        msg: "Hi There"
-    });
+    
 });
 
 app.post('/todo', function(req, res){
